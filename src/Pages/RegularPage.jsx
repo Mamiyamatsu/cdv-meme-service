7bc0f05b0c9ae4f0305bcd37feea78066d2ca9c4
silenesei <marcelina.klimczewska@gmail.com>
2025-06-09 18:26:27 +0200

    DRY - all logic in one component

--- src/Pages/RegularPage.jsx
+++ src/Pages/RegularPage.jsx
@@ -1,22 +1,10 @@
-import { useSelector, useDispatch } from "react-redux";
-import { toggleFavourite, upvoteMeme, downvoteMeme } from "../store/actions";
-import { MemeComponent } from "../Components/MemeComponent";
+import { MemesPage } from "./MemesPage";
 
 export function RegularPage() {
-  const memes = useSelector((state) => state.memes);
-  const dispatch = useDispatch();
-
-
-  const regularMemes = memes.filter((meme) => meme.upvotes > 2 && meme.upvotes < 10);
-
   return (
-    <div>
-       <MemeComponent
-        memes={regularMemes}
-        handleUpvote={(id) => dispatch(upvoteMeme(id))}
-        handleDownvote={(id) => dispatch(downvoteMeme(id))}
-        toggleFavourite={(id) => dispatch(toggleFavourite(id))}
-      />
-    </div>
+    <MemesPage
+      filterFn={(meme) => meme.upvotes > 2 && meme.upvotes < 10}
+      emptyMessage="Brak zwykłych memów!"
+    />
   );
 }
--- src/Pages/WaitingRoomPage.jsx
+++ src/Pages/WaitingRoomPage.jsx
@@ -1,22 +1,10 @@
-import { useSelector, useDispatch } from "react-redux";
-import { toggleFavourite, upvoteMeme, downvoteMeme } from "../store/actions";
-import { MemeComponent } from "../Components/MemeComponent";
+import { MemesPage } from "./MemesPage";
 
 export function WaitingRoomPage() {
-  const memes = useSelector((state) => state.memes);
-  const dispatch = useDispatch();
-
-
-  const waitingRoomMemes = memes.filter((meme) => meme.upvotes <= 2);
-
   return (
-    <div>
-       <MemeComponent
-        memes={waitingRoomMemes}
-        handleUpvote={(id) => dispatch(upvoteMeme(id))}
-        handleDownvote={(id) => dispatch(downvoteMeme(id))}
-        toggleFavourite={(id) => dispatch(toggleFavourite(id))}
-      />
-    </div>
+    <MemesPage
+      filterFn={(meme) => meme.upvotes <= 2}
+      emptyMessage="Brak memów w poczekalni!"
+    />
   );
 }
