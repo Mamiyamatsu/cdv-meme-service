dda4c5122a18f5f8e6835da56ca704f0390030c6
Mamiyamatsu <szalewskifilip@gmail.com>
2025-06-09 18:45:20 +0200

    Uproszczenie NAvigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -6,13 +6,12 @@ export function Navigation() {
     return (
     <nav className='site-nav'>
         <div className='site-nav-content'>
-            <NavLink to="/" className={({isActive}) => isActive ? 'site-active page-main-active' : 'site-nav-link'}>GŁÓWNA</NavLink>
-            <NavLink to="/hot" className={({isActive}) => isActive ? 'site-active page-hot-active' : 'site-nav-link'}>HOT</NavLink>
+            <NavLink to="/" className="site-nav-link">GŁÓWNA</NavLink>
+            <NavLink to="/hot" className="site-nav-link">HOT</NavLink>
             <div className="site-portal-name">BOOMERZY.PL</div>
-            <NavLink to="/poczekalnia" className={({isActive}) => isActive ? 'site-active page-waiting-active' : 'site-nav-link'}><Hourglass size={20} /></NavLink>
-            <NavLink to="/dodaj" className={({isActive}) => isActive ? 'site-active page-add-active' : 'site-nav-link'}><Plus size={20} /></NavLink>
-            <NavLink to="/favourites" className={({isActive}) => isActive ? 'site-active page-fav-active' : 'site-nav-link'}><Star size={20} /></NavLink>
-        
+            <NavLink to="/poczekalnia" className="site-nav-link"><Hourglass size={20} /></NavLink>
+            <NavLink to="/dodaj" className="site-nav-link"><Plus size={20} /></NavLink>
+            <NavLink to="/favourites" className="site-nav-link"><Star size={20} /></NavLink>     
         </div>
     </nav>
     );
